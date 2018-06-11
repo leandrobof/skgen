0fec7eb9ac3c084ea2409a69f15e93c144f94a9d
leandrobof <leandrobof@hotmail.com>
2018-06-11 15:21:58 +0000

    Update run.sh

--- examples/W/run.sh
+++ examples/W/run.sh
@@ -2,7 +2,7 @@ sed -i 's/Udd  Upp  Uss/0.2  0.2  0.2/' W-W.skf
 sed -i 's/-0.0473147/0.040/' W-W.skf
 
 cp bulk dftb_in.hsd
-../dftb+
+dftb+
 dp_bands band.out w_band
 efermi=`grep "Fermi energy" detailed.out | awk '{print $5}' `
 python plot2.py $efermi
