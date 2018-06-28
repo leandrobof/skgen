36016e4de11e46f0344064f44a0f5e9f4da68431
leandrobof <leandrobof@hotmail.com>
2018-06-27 22:06:56 -0300

    Add files via upload

--- examples/BN/plot2.py
+++ examples/BN/plot2.py
@@ -1,19 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import os
 
 
-a=np.loadtxt("BN_band.dat")
-b=np.loadtxt("BN_band_tot.dat")
-r=range(0,len(b[:,0]))
-Efermi=sys.argv[1]
-efermi=float(b[240,-9])*np.ones(np.shape(b))
+os.system("cp bulk dftb_in.hsd"  )
+os.system("dftb+"  )
+os.system("dp_bands band.out band"  )
+b=np.loadtxt("BN_band.dat")
+for line in open("detailed.out","r"):
+    if "Fermi energy" in line:
+        Efermi=line.split()[4]
 
 
-for i in range(2,len(a[0,:])):
-    plt.plot(r,a[:,i],color="r")
+a=np.loadtxt("band_tot.dat")
 
-for j in range(3,len(b[0,:-8])):
-    plt.plot(r,(b[:,j]-efermi[:,1]),color="b")
+
+efermi=float(a[240,-9])*np.ones(np.shape(b))
+b=np.sort(b)
+
+for i in range(0,len(b[0,:])):
+    plt.plot(a[:,0],(b[:,i]),color="r")
+
+for j in range(1,len(a[0,:])):
+    plt.plot(a[:,0],a[:,j]-efermi[:,1],color="b")
 
 plt.show()
